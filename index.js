ef1db15d44eb43ac164652a1a4eb8a3d39db4bb9
jbrichardet <jbrichardet.openbridge@tf1.fr>
2015-05-29 11:07:37 +0200

    Refacto + quality selector

--- index.js
+++ index.js
@@ -1,38 +1,95 @@
+'use strict';
+
 var request = require('request');
 var cheerio = require('cheerio');
 var S = require('string');
 
+function _validateQuality(targets, quality){
+	var i = 0;
+	while(targets[i] !== quality && i < targets.length){
+		i++;
+	}
 
-var urlRoot = "https://eztv.ch/";
-//var urlRoot = "https://eztv-proxy.net/";
+	return i < targets.length;
+}
 
+function _getValidQualities(target){
+	// No quality criteria
+	if(!target){
+		return null;
+	}
 
-var self = module.exports;
+	// User already provided an array
+	if(Array.isArray(target))
+		return target;
 
-self.getShows = function(options, callback) {
-	
-	request(urlRoot + "showlist/", function (error, response, body) {
+	var targetArray;
+	var plusRegExp = target.match(/([^+]+)/);
+	// User provided a comma separated list
+	if(target.indexOf(',') > 0){
+		targetArray = target.split(',');
+		targetArray.forEach(function(quality, index){
+			targetArray[index] = quality.trim();
+		});
+	}
+	// User provided a minimum qualiy in a '+ form' (HDTV+, 720p+,...)
+	else if(plusRegExp){
+		var base = plusRegExp[1];
+		targetArray = [base];
+		switch(base){
+			case 'HDTV':
+				targetArray.push('720p');
+				targetArray.push('1080p');
+				break;
+			case '720p':
+				targetArray.push('1080p');
+				break;
+			default:
+		}
+	}
+	else{
+		targetArray = target;
+	}
+	console.log(targetArray);
+	return targetArray;
+}
 
-		if (!error && response.statusCode == 200) {
+var EZTVClient = module.exports = function EZTVClient(url){
+	if(!url)
+		this.url = "https://eztv.ch";
+	else
+		this.url = url;
+};
 
+function Show(host){
+	this.host = host;
+}
+
+EZTVClient.prototype.getShows = function(options, callback) {
+	request(this.url + "/showlist/", function (error, response, body){
+		if(response.statusCode !== 200)
+			return callback(new Error("Error while loading URL "+this.host + this.url+", receiving code "+response.statusCode));
+		if (!error) {
 			var list = [];
 			var $ = cheerio.load(body);
-			var $elements = $("table.forum_header_border tr[name=hover]");
+			var $elements = $("table.forum_header_border tr[name=hover]").filter(function(){
+				var title = $(this).find("td").eq(1).text();
+				return title.toLowerCase().search(options.query.toLowerCase()) >= 0;
+			});
 			$elements.each(function(i, e) {
-				
-				var show = {};
+
+				var show = new Show(this.url);
 				show.url = $(e).find("td").eq(1).find("a").attr("href");
 				if (!show.url) {
-					return;
+					return callback(new Error("Unable to find URL for this show"));
 				}
 				var regex = show.url.match(/\/shows\/(\d+)\/([^\/]+)/);
 				if (!regex) {
-					//console.log("Unparsed show: " + show.url);
-					return;
+					return callback(new Error("URL of the show is not in a supported format", show.url));
 				}
 				show.id = parseInt(regex[1]);
 				show.slug = regex[2];
-				
+
 				var title = $(e).find("td").eq(1).text();
 				if (S(title).endsWith(", The")) {
 					title = "The " + S(title).chompRight(", The").s;
@@ -40,55 +97,45 @@ self.getShows = function(options, callback) {
 				show.title = title;
 				show.status = $(e).find("td").eq(2).find("font").attr("class");
 
-				if (options && options.query) {
-					if (show.title.toLowerCase().search(options.query.toLowerCase()) >= 0) {
-						//console.log(show.title);
-						list.push(show);
-					}
-				}
-				else {
-					list.push(show);						
-				}
-				
-			});
+				list.push(show);
 
-			if (callback) callback(null, list);
-		
+			}.bind(this));
+
+			if(list.length > 0){
+				callback(null, list);
+			}
+			else {
+				// TODO should retry
+				callback(new Error('No episode found for query: ', options.query));
+			}
 		}
-		else {
-			console.log("Error getting shows", error, response);
-			if (callback) callback(new Error("Error getting shows"), null);
+		else{
+			callback(error);
 		}
-	});
+	}.bind(this));
 
 };
 
-self.getShowEpisodes = function(showId, callback) {
-	
-	request(urlRoot + "shows/" + showId + "/", function (error, response, body) {
-
-		if (!error && response.statusCode == 200) {			 
-			
-			var result = {
-				id: showId,
-				episodes: []
-			};
-			
+Show.prototype.getShowEpisodes = function(qualityRequest, callback) {
+	var targetedQualities = _getValidQualities(qualityRequest);
+	request(this.host + this.url, function (error, response, body) {
+		if(response.statusCode !== 200)
+			return callback(new Error("Error while loading URL "+this.host + this.url+", receiving code "+response.statusCode));
+		if (!error) {
+			var episodes = [];
 			var $ = cheerio.load(body);
-			result.title = $("td.section_post_header").eq(0).find("b").text();			
-			
+
 			var $episodes = $("table.forum_header_noborder tr[name=hover]");
 			$episodes.each(function(i, e) {
-			
 				var episode = {};
-				
+
 				episode.url = $(e).find("td").eq(1).find("a").attr("href");
 				if (!episode.url) return;
 				var urlRegex = episode.url.match(/\/ep\/(\d+)\/.*/);
 				if (urlRegex) {
-					episode.id = parseInt(urlRegex[1]);					
+					episode.id = parseInt(urlRegex[1]);
 				}
-				
+
 				episode.title = $(e).find("td").eq(1).find("a").text();
 				var titleRegex = episode.title.match(/(.+) s?(\d+)[ex](\d+)(e(\d+))?(.*)/i);
 				if (titleRegex) {
@@ -97,27 +144,26 @@ self.getShowEpisodes = function(showId, callback) {
 					episode.episodeNumber = parseInt(titleRegex[3]);
 					episode.episodeNumber2 = parseInt(titleRegex[5]);
 					episode.extra = titleRegex[6].trim();
+					var quality = episode.extra.match(/(HDTV|720p|1080p)/);
+					episode.quality = quality ? quality[1] : "unknown";
 					episode.proper = episode.extra.toLowerCase().indexOf("proper") >= 0;
 					episode.repack = episode.extra.toLowerCase().indexOf("repack") >= 0;
 				}
 				else {
-					//console.log("unparsed episode: " + episode.title);
+					return callback(new Error("Title of the episode is not in a supported format", episode.title));
 				}
-				
+
 				episode.magnet = $(e).find("td").eq(2).find("a.magnet").attr("href");
 				episode.torrentURL = $(e).find("td").eq(2).find("a.download_1").attr("href");
-				
-				result.episodes.push(episode);						
-				
-			});
 
-			if (callback) callback(null, result);
-		
+				if(!targetedQualities || _validateQuality(targetedQualities, episode.quality))
+					episodes.push(episode);
+
+			});
+			callback(null, episodes);
 		}
 		else {
-			if (callback) callback(new Error("Error getting show episodes"), null);
+			callback(error, null);
 		}
-	});
-	
-}
-
+	}.bind(this));
+};
