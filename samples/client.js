9d69c231203c174149799b413baccc68289a01ca
JbIPS <jeanbaptiste.richardet@gmail.com>
2016-04-11 17:16:27 +0200

    Refacto data model

--- samples/client.js
+++ samples/client.js
@@ -4,8 +4,9 @@ const EZTV = require('./lib/eztvClient');
 const util = require('util');
 
 let eztv = new EZTV();
-eztv.getShows({query: 'conan'})
+eztv.getTodayReleases()
 .then(function(results){
-	console.log(results);
+	results.filter(ep => ep.show.title.match('John Oliver'))
+  .forEach(ep => console.log(ep));
 })
 .catch(console.error);
